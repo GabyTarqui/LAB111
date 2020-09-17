8f18c9efb49e21858e1b4d9752dcab4019302573
GabyTarqui <70592339+GabyTarqui@users.noreply.github.com>
2020-09-17 11:18:43 -0400

    Update Ejercicio2.py

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -3,10 +3,13 @@ a=int(input("Ingrese el valor de a: "))
 b=int(input("Ingrese el valor de b: "))
 c=int(input("Ingrese el valor de c: "))
 n=(b**2)-(4*a*c)
-if n>0:
-    print("Las raices serán dos números reales y diferentes")
-else:
-    if n<0:
-        print("Las raices serán dos números complejos")
+if(a!=0): 
+    if n>0:
+        print("Las raices serán dos números reales y diferentes")
     else:
-        print("Las raices serán dos números reales e iguales")
+        if n<0:
+            print("Las raices serán dos números complejos")
+        else:
+            print("Las raices serán dos números reales e iguales")
+else:
+    print("No es una ecuación de segundo grado")
